b3c6ec7ed65384c75d74fb65da86982ae762981f
Lee Benzacar <LMBenzacar@gmail.com>
2022-03-17 03:56:40 +0000

    Product_details_spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-  
-  # SETUP
+
+RSpec.feature "Visitor clicks on a product in home page", type: :feature, js: true do
+
   before :each do
     @category = Category.create! name: 'Apparel'
+
     10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
@@ -14,10 +15,14 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       )
     end
   end
-  scenario "They see all products" do
-    # ACT
+
+  scenario "They see product details page" do
     visit root_path
 
-    expect(page).to have_css "article.product", count: 10
+    find(".product:nth-of-type(1) img").click
+
+    expect(page).to have_content("Description")
+    expect(page).to have_content("Quantity")
   end
+
 end
